4c11019f2199dd01d82fdaa5de695db3a7db2030
Piero Ribeiro <piero.webmaster@gmail.com>
2020-03-28 01:09:04 -0300

    melhorias

--- gulpfile.js
+++ gulpfile.js
@@ -1,64 +1,99 @@
-const gulp          = require('gulp');
-const sass          = require('gulp-sass');
-const typescript    = require('gulp-typescript');
-const concat        = require('gulp-concat');
-const minify        = require('gulp-minify-css');
-const merge         = require('merge-stream');
-const rename        = require('gulp-rename');
+const gulp                      = require('gulp')
+const sass                      = require('gulp-sass')
+const typescript                = require('gulp-typescript')
+const concat                    = require('gulp-concat')
+const minifycss                 = require('gulp-minify-css')
+const minifyjs                  = require('gulp-minify')
+const merge                     = require('merge-stream')
+const rename                    = require('gulp-rename')
+const browserSync               = require('browser-sync').create()
 
-const copyNecessaryFiles = (cb) => {
+
+function copyNecessaryFiles(cb) {
     gulp.src('./node_modules/requirejs/require.js')
+        .pipe(minifyjs({
+            ext: '.min.js',
+            noSource: true
+        }))
         .pipe(gulp.dest('./dist/assets/js/'));
 
     cb()
 }
 
-const copyHTMLFiles = (cb) => {
+function copyHTMLFiles(cb) {
     gulp.src('./src/html/**/*.html')
         .pipe(gulp.dest('./dist/'));
 
     cb()
 }
 
-const generateJSFiles = (cb) => {
+function generateJSFiles(cb) {
     gulp.src('src/**/*.ts')
         .pipe(typescript({
-            // typescript: require('typescript'),
-            // target: 'ES6',
-            // module: 'system',
-            // experimentalDecorators: false,
-            // emitDecoratorMetadata: false,
-            // outFile: 'script.js'
             module: 'amd',
             noImplicitAny: true,
             outFile: 'script.js'
         }))
-        .pipe(gulp.dest('./dist/assets/js'))    
+        .pipe(minifyjs({
+            ext: '.min.js',
+            noSource: true
+        }))
+        .pipe(gulp.dest('./dist/assets/js'))
+
     cb()
 }
 
-const generateCSSFiles = (cb) => {
+function generateCSSFiles(cb) {
     let scssStream = gulp.src('src/sass/**/*.scss')
         .pipe(sass())
         .pipe(concat('style.scss'))
 
     merge(scssStream)
         .pipe(concat('styles.css'))
-        .pipe(minify())
+        .pipe(minifycss())
         .pipe(rename({suffix: '.min'}))
         .pipe(gulp.dest('dist/assets/css'))
-    
+
     cb()
 }
 
-exports.default = (done) => {
-    copyNecessaryFiles(() => {
-        copyHTMLFiles(() => {
+function openBrowser(cb) {
+    browserSync.init({
+        server: {
+            baseDir: './dist'
+        },
+    })
+
+    cb()
+}
+
+function watch() {
+    gulp.watch('./src/sass/**/*.scss', generateCSSFiles)
+    gulp.watch('./src/classes/**/*.ts', generateJSFiles)
+    gulp.watch('./src/html/**/*.html', copyHTMLFiles)
+    gulp.watch('./dist/**/*.css').on('change', browserSync.reload)
+    gulp.watch('./dist/**/*.js').on('change', browserSync.reload)
+    gulp.watch('./dist/**/*.html').on('change', browserSync.reload)
+}
+
+function _default() {
+    copyNecessaryFiles(()=>{
+        copyHTMLFiles(()=>{
             generateJSFiles(()=>{
                 generateCSSFiles(()=>{
-                    done()
+                    openBrowser(()=>{
+                        watch()          
+                    })
                 })
             })
         })
     })
 }
+
+exports.default                 = _default
+exports.copyNecessaryFiles      = copyNecessaryFiles
+exports.copyHTMLFiles           = copyHTMLFiles
+exports.generateJSFiles         = generateJSFiles
+exports.generateCSSFiles        = generateCSSFiles
+exports.openBrowser             = openBrowser
+exports.watch                   = watch
--- src/classes/index.ts
+++
@@ -1,9 +0,0 @@
-import top          = require("./top/index")
-import live         = require("./live/index")
-import destaques    = require("./destaques/index")
-import playlists    = require("./playlists/index")
-import search       = require("./search/index")
-import footer       = require("./footer/index")
-
-let _top = new top.Top()
-console.log(_top.teste())
